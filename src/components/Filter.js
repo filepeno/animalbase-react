6b3bff55c737fc881760d0ba16d9889042de3c3b
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-16 18:44:00 +0100

    renamed classes with className

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,13 +2,13 @@ export default function Filter() {
   return (
     <>
       Filter:
-      <button class="filter" data-action="filter" data-filter="cat">
+      <button className="filter" data-action="filter" data-filter="cat">
         Only Cats
       </button>
-      <button class="filter" data-action="filter" data-filter="dog">
+      <button className="filter" data-action="filter" data-filter="dog">
         Only Dogs
       </button>
-      <button class="filter" data-action="filter" data-filter="*">
+      <button className="filter" data-action="filter" data-filter="*">
         all
       </button>
     </>
