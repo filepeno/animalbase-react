5f03dca0d10660f06e478de71d39fb93fad842e5
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-17 10:15:07 +0100

    function to change star status based on state

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,9 +1,20 @@
+// import AddStar from "./ChangeStarStatus";
+import { useState } from "react";
+
 export default function Animal(props) {
-  console.log(props);
+  const initialStatus = false;
+  const [status, changeStarStatus] = useState(initialStatus);
+  function clickOnStar() {
+    console.log("status changed");
+    changeStarStatus((status) => (status ? false : true));
+  }
+
   return (
     <tr>
       <td data-field="winner" data-winner="false"></td>
-      <td data-field="star">⭐</td>
+      <td onClick={clickOnStar} data-field="star">
+        {status ? "⭐" : "☆"}
+      </td>
       <td data-field="name">{props.name}</td>
       <td data-field="type">{props.type}</td>
       <td data-field="desc">{props.desc}</td>
---
+++ src/components/ChangeStarStatus.js
@@ -0,0 +1,6 @@
+// import { useState } from "react";
+
+// export default function ChangeStarStatus(e) {
+//   console.log(e);
+//   const [status, changeStatus] = useState(false);
+// }
